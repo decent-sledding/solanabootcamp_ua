af71db3459224932ab2c43ccab2973246b36a55f
bitwunder <ethereum51@gmail.com>
2024-05-18 13:18:57 +0300

    refactor: update getbalance in lab_2

--- lab_2/src/getbalance.ts
+++ lab_2/src/getbalance.ts
@@ -5,21 +5,27 @@ import {
     PublicKey,
     clusterApiUrl,
 } from "@solana/web3.js";
-import { get_keys_from_env_or_cli, get_logger } from 'sharedlibs';
+import { get_keys_from_env_or_cli, get_balance, Balance, get_logger } from 'sharedlibs';
 
 
 const keypair = get_keys_from_env_or_cli().parse().parseKeys();
 const log = get_logger();
-
 const connection = new Connection(clusterApiUrl('devnet'));
 const publicKey = keypair.publicKey;
 
-const lamports = await connection.getBalance(publicKey);
-const solports = lamports / LAMPORTS_PER_SOL;
+
+if (!publicKey) {
+    throw Error("Could not read the public key");
+}
+
+const balance: Balance = await getBalance(connection, publicKey);
 
 
+log(`Using public key: ${publicKey}`);
 log('Connected to the DevNet');
 log(`Account's public key:\n\t${publicKey}`);
 log("Balance:")
-log(`\tLamports: ${lamports}`);
-log(`\tSOL: ${solports}`);
+log(`\tLamports: ${balance.getLamps()}`);
+log(`\tSOL: ${balance.getSOL()}`);
+
+
